2899d9d41fc093e47e9b8b475184632e6c8c0680
fl-air <91276549+fl-air@users.noreply.github.com>
2021-05-03 15:07:42 +0000

    obsolete filters

--- back/database/DDL/airbnb.sql
+++ back/database/DDL/airbnb.sql
@@ -16,3 +16,57 @@ ALTER TABLE Users
 
 -- Users Table Create SQL
 CREATE TABLE Rooms
+(
+    `id`         INT          NOT NULL AUTO_INCREMENT COMMENT 'id',
+    `image`      VARCHAR(45)  NULL COMMENT 'image',
+    `type`       INT          NOT NULL COMMENT 'type',
+    `title`      VARCHAR(45)  NOT NULL COMMENT 'title',
+    `location`   VARCHAR(100) NOT NULL COMMENT 'location',
+    `price`      INT          NOT NULL COMMENT 'price',
+    `capacity`   INT          NOT NULL COMMENT 'capacity',
+    `showerRoom` INT          NOT NULL COMMENT 'showerRoom',
+    `options`    INT          NOT NULL COMMENT 'options',
+    `host`       INT          NOT NULL COMMENT 'host',
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE Rooms
+    COMMENT 'Rooms';
+
+ALTER TABLE Rooms
+    ADD CONSTRAINT FK_Rooms_host_Users_id FOREIGN KEY (host)
+        REFERENCES Users (id) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+
+-- Users Table Create SQL
+CREATE TABLE Reservations
+(
+    `id`       INT  NOT NULL AUTO_INCREMENT COMMENT 'id',
+    `guest`    INT  NOT NULL COMMENT 'guest',
+    `checkIn`  DATE NOT NULL COMMENT 'checkIn',
+    `checkOut` DATE NOT NULL COMMENT 'checkOut',
+    `roomId`   INT  NOT NULL COMMENT 'roomId',
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE Reservations
+    COMMENT 'Reservatsions';
+
+ALTER TABLE Reservations
+    ADD CONSTRAINT FK_Reservations_guest_Users_id FOREIGN KEY (guest)
+        REFERENCES Users (id) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+ALTER TABLE Reservations
+    ADD CONSTRAINT FK_Reservations_roomId_Rooms_id FOREIGN KEY (roomId)
+        REFERENCES Rooms (id) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+
+-- Users Table Create SQL
+CREATE TABLE Options
+(
+    `id`   INT         NOT NULL AUTO_INCREMENT COMMENT 'id',
+    `name` VARCHAR(20) NOT NULL COMMENT 'name',
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE Options
