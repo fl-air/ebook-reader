4ed0967ef671c06cb4d6a9745614e2305d3d6789
fl-air <91276549+fl-air@users.noreply.github.com>
2021-05-09 11:02:45 +0000

    branch merge

--- back/database/index.js
+++ back/database/index.js
@@ -1,3 +1,4 @@
 require('dotenv').config();
 import {DataTypes, Sequelize} from 'sequelize';
-import {userModel} from "./models/userMo
+import {userModel} from "./models/userModel";
+import {roomModel} from "./models/roomModel";
