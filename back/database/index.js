0fe75b0c85b04f7b6126613379038d16e2e18a14
fl-air <91276549+fl-air@users.noreply.github.com>
2021-05-09 11:41:28 +0000

    update circle.yml

--- back/database/index.js
+++ back/database/index.js
@@ -2,3 +2,6 @@ require('dotenv').config();
 import {DataTypes, Sequelize} from 'sequelize';
 import {userModel} from "./models/userModel";
 import {roomModel} from "./models/roomModel";
+import {reservationModel} from "./models/reservationModel";
+
+const
