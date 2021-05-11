3053a87757156e5004bf99086adf67173477f6e6
fl-air <91276549+fl-air@users.noreply.github.com>
2021-05-11 10:07:59 +0000

    Client update

--- back/database/index.js
+++ back/database/index.js
@@ -4,4 +4,4 @@ import {userModel} from "./models/userModel";
 import {roomModel} from "./models/roomModel";
 import {reservationModel} from "./models/reservationModel";
 
-const sequelize = new Sequelize(process.env.DB_DATAB
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_USER_P
