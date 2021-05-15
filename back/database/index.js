dfd1d678101da09f930fc057fb188bb024146f32
fl-air <91276549+fl-air@users.noreply.github.com>
2021-05-15 07:14:53 +0000

    bugs fix

--- back/database/index.js
+++ back/database/index.js
@@ -4,4 +4,6 @@ import {userModel} from "./models/userModel";
 import {roomModel} from "./models/roomModel";
 import {reservationModel} from "./models/reservationModel";
 
-const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_USER_P
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_USER_PASSWORD, {
+    dialect: 'mysql',
+    host: process.env.DB_HO
