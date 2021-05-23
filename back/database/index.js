02f4ba4d5ab969e1472ae72275067a22c59b25ba
fl-air <91276549+fl-air@users.noreply.github.com>
2021-05-23 10:35:51 +0000

    Fix version string

--- back/database/index.js
+++ back/database/index.js
@@ -6,4 +6,8 @@ import {reservationModel} from "./models/reservationModel";
 
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_USER_PASSWORD, {
     dialect: 'mysql',
-    host: process.env.DB_HO
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    pool: {
+        max: 5,
+ 
