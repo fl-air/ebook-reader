ee98b7d50522081c68ac452e3f7489066a2bf149
fl-air <91276549+fl-air@users.noreply.github.com>
2021-07-04 13:12:20 +0000

    update bindings

--- back/database/migrations/20191016073712-create-room.js
+++ back/database/migrations/20191016073712-create-room.js
@@ -3,4 +3,6 @@ module.exports = {
     up: (queryInterface, Sequelize) => {
         return queryInterface.createTable('Rooms', {
             id: {
-   
+                allowNull: false,
+                autoIncrement: true,
+      
