ea1c096d54263fda6064dfd7a5584d894c9e8801
fl-air <91276549+fl-air@users.noreply.github.com>
2021-07-11 11:29:11 +0000

    Pontoon: Update Italian (it) localization of Firefox Screenshots

--- back/database/migrations/20191016073712-create-room.js
+++ back/database/migrations/20191016073712-create-room.js
@@ -17,4 +17,6 @@ module.exports = {
             },
             title: {
                 allowNull: false,
-       
+                type: Sequelize.STRING
+            },
+            location: {
