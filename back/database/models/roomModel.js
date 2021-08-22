e44e3aec53a901a76e992b931942208fe13f7dc2
fl-air <91276549+fl-air@users.noreply.github.com>
2021-08-22 09:58:19 +0000

    The beginning

--- back/database/models/roomModel.js
+++ back/database/models/roomModel.js
@@ -16,3 +16,6 @@ function roomModel(sequelize, DataTypes) {
             type: DataTypes.STRING,
             allowNull: false,
         },
+        price: {
+            type: DataTypes.INTEGER,
+ 
