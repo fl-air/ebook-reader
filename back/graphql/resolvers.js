7dbb8bcd690c5bee8576af63f762137870fcad11
fl-air <91276549+fl-air@users.noreply.github.com>
2021-09-26 22:05:16 +0000

    commit...

--- back/graphql/resolvers.js
+++ back/graphql/resolvers.js
@@ -3,4 +3,6 @@ import {db} from "../database/index";
 const {gte, lte, ne, in: opIn, notIn, and, or, between} = db.Sequelize.Op;
 
 const resolvers = {
-       
+        Query: {
+            getUsers: () => db.User.findAll(),
+  
