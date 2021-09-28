5f2ed269cd78e2c368f3117d72a3941c39b165e9
fl-air <91276549+fl-air@users.noreply.github.com>
2021-09-28 08:51:50 +0000

    Version 3.5.1

--- back/graphql/resolvers.js
+++ back/graphql/resolvers.js
@@ -5,4 +5,5 @@ const {gte, lte, ne, in: opIn, notIn, and, or, between} = db.Sequelize.Op;
 const resolvers = {
         Query: {
             getUsers: () => db.User.findAll(),
-  
+            getUserById: (_, {id}) => db.User.findByPk(id),
+ 
