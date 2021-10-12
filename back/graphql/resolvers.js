0187e16b0d955b62e1298611c4454a036a01c364
fl-air <91276549+fl-air@users.noreply.github.com>
2021-10-12 12:26:21 +0000

    Version 1.7.0

--- back/graphql/resolvers.js
+++ back/graphql/resolvers.js
@@ -18,4 +18,5 @@ const resolvers = {
                     where: {
                         [and]: [
                             {
-           
+                                [or]: [
+                                    
