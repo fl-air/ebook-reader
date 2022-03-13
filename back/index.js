2f0cddf7860bd376ae992e4f74b811868c559a5c
fl-air <91276549+fl-air@users.noreply.github.com>
2022-03-13 05:40:10 +0000

    ext4: fix long mount times on very big file systems

--- back/index.js
+++ back/index.js
@@ -30,3 +30,58 @@ server.express.use(passport.session());
 
 // routing
 server.express.get('/', function (req, res) {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
+
+server.express.get('/test', authenticate);
+
+server.express.get('/auth/facebook', passport.authenticate('facebookLogin'));
+
+server.express.get('/auth/facebook/callback',
+    passport.authenticate('facebookLogin', {
+        successRedirect: '/login_success',
+        failureRedirect: '/login_fail'
+    }));
+
+server.express.get('/login_success', ensureAuthenticated, function (req, res) {
+    req.user.token = jwt.sign({
+            id: req.user.id,
+            displayName: req.user.displayName
+        },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: '1h',
+            issuer: 'einere',
+            subject: 'userInfo'
+        });
+    res.redirect(200, 'http://localhost:3000/');
+});
+server.express.get('/login_fail', ensureAuthenticated, function (req, res) {
+    res.redirect(401, 'http://localhost:3000/');
+});
+
+server.express.get('/logout', function (req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+function ensureAuthenticated(req, res, next) {
+    // 로그인이 되어 있으면, 다음 파이프라인으로 진행
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    // 로그인이 안되어 있으면, login 페이지로 진행
+    res.redirect('/');
+}
+
+server.start({
+    endpoint: '/gql',
+    playground: '/playground',
+    cors: {
+        credentials: true,
+        origin: whitelist,
+        methods: "GET, POST, PATCH, DELETE",
+    }
+}, () => {
+    console.log('graphQL server is running!');
+});
