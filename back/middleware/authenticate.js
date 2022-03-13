95544a8a7d6cb224264aa3043a05adb3a54e44e0
fl-air <91276549+fl-air@users.noreply.github.com>
2022-03-13 07:47:05 +0000

    change permalink

--- back/middleware/authenticate.js
+++ back/middleware/authenticate.js
@@ -1,2 +1,16 @@
 
 import jwt from "jsonwebtoken";
+
+export default (req, res, next) => {
+    if (req.isAuthenticated()) {
+        try {
+            const decoded = jwt.verify(req.user.token, process.env.JWT_SECRET, {
+                issuer: 'einere'
+            });
+            if (decoded) res.status(200).send(true);
+            else res.status(403).send(false);
+        } catch (e) {
+            res.status(403).send(e);
+        }
+    } else res.status(401).send(false);
+};
