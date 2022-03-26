18499d427c947a0f1c3b321a880fc0464a5666ad
fl-air <91276549+fl-air@users.noreply.github.com>
2022-03-26 06:36:02 +0000

    update

--- back/middleware/passport-facebook.js
+++ back/middleware/passport-facebook.js
@@ -1,2 +1,5 @@
 const passport = require('passport');
-const FacebookStrategy = require('passport-facebook').S
+const FacebookStrategy = require('passport-facebook').Strategy;
+const {db} = require('../database/index');
+
+p
