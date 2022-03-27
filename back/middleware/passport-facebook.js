f2c6ef21bf1cdddaf8bdcfaf02ed80be542a0ec1
fl-air <91276549+fl-air@users.noreply.github.com>
2022-03-27 22:18:45 +0000

    drivers: cpufreq: Send a uevent when governor changes

--- back/middleware/passport-facebook.js
+++ back/middleware/passport-facebook.js
@@ -2,4 +2,5 @@ const passport = require('passport');
 const FacebookStrategy = require('passport-facebook').Strategy;
 const {db} = require('../database/index');
 
-p
+passport.serializeUser((user, done) => {
+    con
