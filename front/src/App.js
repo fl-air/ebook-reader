b3b00faf676ab0e5b2530c2125861fa0996822d1
fl-air <91276549+fl-air@users.noreply.github.com>
2022-07-09 19:56:12 +0000

    netlink: rate-limit leftover bytes warning and print process name

--- front/src/App.js
+++ front/src/App.js
@@ -1,2 +1,32 @@
 
 import React from 'react';
+import './stylesheet/App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import client from './apollo/apolloClient';
+import {ApolloProvider} from '@apollo/react-hooks';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Home from './routes/Home';
+import User from './routes/User';
+import Header from "./components/Header";
+
+function App() {
+    return (
+        <ApolloProvider client={client}>
+            <div className="App">
+                <BrowserRouter>
+                    <Header/>
+                    <Switch>
+                        <Route path="/user">
+                            <User/>
+                        </Route>
+                        <Route path="/">
+                            <Home/>
+                        </Route>
+                    </Switch>
+                </BrowserRouter>
+            </div>
+        </ApolloProvider>
+    );
+}
+
+export default App;
