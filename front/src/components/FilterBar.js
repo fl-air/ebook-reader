63f6c3d455c9ef3fa0d020c8961d9019cb2a2af0
fl-air <91276549+fl-air@users.noreply.github.com>
2022-09-09 14:59:19 +0000

    fixed #15

--- front/src/components/FilterBar.js
+++ front/src/components/FilterBar.js
@@ -1,4 +1,6 @@
 import React, {useContext, useReducer} from 'react';
 import FilterButton from "./FilterButton";
 import style from '../stylesheet/FilterBar.module.css';
-import {FilterContext} from "./context/FilterContext"
+import {FilterContext} from "./context/FilterContext";
+import {QueryContext} from "./context/QueryContext";
+import dateFil
