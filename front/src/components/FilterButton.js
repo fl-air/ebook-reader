5e3242f0f5575b130f0eac5088694f31e6f50d56
fl-air <91276549+fl-air@users.noreply.github.com>
2022-09-22 22:10:22 +0000

    - work on dbus interface

--- front/src/components/FilterButton.js
+++ front/src/components/FilterButton.js
@@ -27,3 +27,45 @@ function FilterButton(props) {
         client.query({
             query: getProperQuery(filterType),
             variables: getProperQueryParameter(filterType, state)
+        })
+            .then(result => {
+                // 쿼리 수행 후 queryDispatch를 이용해 queryState를 업데이트
+                queryDispatch({type: 'update', payload: getProperQueryResult(filterType, result)});
+            })
+            .catch(error => {
+                console.error(error);
+                queryDispatch({type: 'init'});
+            })
+    };
+    const showModal = () => setShow(true);
+    const noShowModal = () => setShow(false);
+
+    // effect
+    useEffect(() => {
+        if (!isEqual(state, initData)) setTitle(getModalTitle(filterType, state));
+        else setTitle(filterType);
+    }, [state]);
+
+    return (
+        <>
+            <button style={style} className={style.FilterButton} onClick={showModal}>{title}</button>
+
+            <Modal show={show} onHide={triggerQuery} animation={true} size={'lg'}>
+                <Modal.Header closeButton>
+                    <Modal.Title>{filterType}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>{props.children}</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={deleteFilterValue}>
+                        delete
+                    </Button>
+                    <Button variant="primary" onClick={triggerQuery}>
+                        save
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
+}
+
+export default FilterButton;
