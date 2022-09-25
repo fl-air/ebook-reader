c045f6c35597862c1b55a662a4761e5b2f3f2d84
fl-air <91276549+fl-air@users.noreply.github.com>
2022-09-25 13:33:38 +0000

    Update ViewModels.cs

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -1,3 +1,7 @@
 import React from 'react';
 import style from '../stylesheet/Header.module.css';
-import {Link} from "react-router-dom"
+import {Link} from "react-router-dom";
+
+function Header() {
+    return (
+        <section style={style}
