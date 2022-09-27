08f9d3e3b4e247e824892840321f3b80513c8b03
fl-air <91276549+fl-air@users.noreply.github.com>
2022-09-27 09:05:47 +0000

    Added title.

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -10,4 +10,4 @@ function Header() {
                     <svg viewBox={'0 0 1000 1000'} role={'presentation'} aria-hidden={true} focusable={false}
                          className={style.logo}>
                         <path
-              
+                            d={"m499.3 736.7c-51-64-81-120.1-91-1
