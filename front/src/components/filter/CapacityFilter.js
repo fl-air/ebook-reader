fa052eddcd9f6f8e11952abe2ce76f6da3acd8ef
fl-air <91276549+fl-air@users.noreply.github.com>
2022-10-09 10:35:24 +0000

    [ Ax ] update source file

--- front/src/components/filter/CapacityFilter.js
+++ front/src/components/filter/CapacityFilter.js
@@ -2,4 +2,6 @@ import React, {useContext} from 'react';
 import {FilterContext} from "../context/FilterContext";
 import style from '../../stylesheet/CapacityFilter.module.css';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faMinus, faPlus} fr
+import {faMinus, faPlus} from '@fortawesome/free-solid-svg-icons'
+
+fu
