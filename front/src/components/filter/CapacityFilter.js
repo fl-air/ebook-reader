40e4cbcae2b12a328702ede8b04fa3454fdf8b45
fl-air <91276549+fl-air@users.noreply.github.com>
2022-10-09 18:28:57 +0000

    Add gitattributes file

--- front/src/components/filter/CapacityFilter.js
+++ front/src/components/filter/CapacityFilter.js
@@ -4,4 +4,5 @@ import style from '../../stylesheet/CapacityFilter.module.css';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faMinus, faPlus} from '@fortawesome/free-solid-svg-icons'
 
-fu
+function CapacityFilter() {
+    const {state, dispatch} = 
