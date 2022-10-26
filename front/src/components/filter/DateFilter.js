56c844a73460922c9ca5bc20de31342cd144c074
fl-air <91276549+fl-air@users.noreply.github.com>
2022-10-26 09:44:36 +0000

    no print

--- front/src/components/filter/DateFilter.js
+++ front/src/components/filter/DateFilter.js
@@ -1,4 +1,6 @@
 import React, {useContext, useState} from "react";
 import moment from 'moment'
 import {DayPickerRangeController, isInclusivelyBeforeDay} from "react-dates";
-import 'react-dates/lib/css/_datepicker.
+import 'react-dates/lib/css/_datepicker.css';
+import 'react-dates/initialize';
+import {Filte
