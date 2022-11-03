a9b4bfc8cb5d6ec5f5a04e618019e4173bff2708
fl-air <91276549+fl-air@users.noreply.github.com>
2022-11-03 08:53:47 +0000

    fixed header

--- front/src/components/filter/PriceFilter.js
+++ front/src/components/filter/PriceFilter.js
@@ -1,4 +1,7 @@
 import React, {useContext, useState} from 'react';
 import {Slider} from '@material-ui/core';
 import style from '../../stylesheet/PriceFilter.module.css';
-import {FilterContext} from "../conte
+import {FilterContext} from "../context/FilterContext";
+
+function PriceFilter(props) {
+    const 
