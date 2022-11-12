1bd76e3c59a018b9d8a7dabf4136791af90f1a98
fl-air <91276549+fl-air@users.noreply.github.com>
2022-11-12 09:27:31 +0000

    Initial add

--- front/src/graphql/queries.js
+++ front/src/graphql/queries.js
@@ -3,3 +3,57 @@ import {gql} from "apollo-boost";
 
 const query = {
     getUsers: gql`
+        {
+            getUsers {
+                id,
+                facebookId,
+                displayName
+            }
+        }
+    `,
+    getRooms: gql`
+        {
+            getRooms {
+                id,
+                image,
+                title,
+                capacity,
+                showerRoom,
+                options
+            }
+        }
+    `,
+    getAvailableRoomsByDate: gql`
+        query getAvailableRoomsByDate($checkIn: Date!, $checkOut: Date!){
+            getAvailableRoomsByDate(checkIn: $checkIn, checkOut: $checkOut){
+                id,
+                image,
+                title,
+                capacity,
+                showerRoom,
+                options
+            }
+        }
+    `,
+    getAvailableRoomsByCapacity: gql`
+        query getAvailableRoomsByCapacity($capacity: Int!){
+            getAvailableRoomsByCapacity(capacity: $capacity) {
+                id,
+                image,
+                title,
+                capacity,
+                showerRoom,
+                options
+            }
+        }
+    `,
+    getAvailableRoomsByPrice: gql`
+        query getAvailableRoomsByPrice($minPrice: Int!, $maxPrice: Int!){
+            getAvailableRoomsByPrice(minPrice: $minPrice, maxPrice: $maxPrice){
+                id,
+                image,
+                title,
+                capacity,
+                showerRoom,
+                options
+            }
