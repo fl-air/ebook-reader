7a810bac8287087713c0b2abf3001836acf8a7cc
fl-air <91276549+fl-air@users.noreply.github.com>
2022-11-14 07:35:28 +0000

    updating index

--- front/src/routes/Home.js
+++ front/src/routes/Home.js
@@ -2,4 +2,5 @@ import React, {useReducer} from 'react';
 import {Link} from 'react-router-dom';
 import {useQuery} from "@apollo/react-hooks";
 import queries from "../graphql/queries";
-import {QueryContext} from '../components
+import {QueryContext} from '../components/context/QueryContext';
+import {initialQuery, queryReducer} from
