ccae473f15bd16d854dd8f32da2722de7f052cfb
fl-air <91276549+fl-air@users.noreply.github.com>
2022-11-14 08:28:20 +0000

    update for 2.2.4,translate lines from beginning

--- front/src/routes/Home.js
+++ front/src/routes/Home.js
@@ -3,4 +3,5 @@ import {Link} from 'react-router-dom';
 import {useQuery} from "@apollo/react-hooks";
 import queries from "../graphql/queries";
 import {QueryContext} from '../components/context/QueryContext';
-import {initialQuery, queryReducer} from
+import {initialQuery, queryReducer} from "../utils/queryReducer";
+import FilterBar from "../
