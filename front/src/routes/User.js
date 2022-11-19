dacdd799737e105c40d13a94192d823e70e3bad9
fl-air <91276549+fl-air@users.noreply.github.com>
2022-11-19 13:33:09 +0000

    more rules

--- front/src/routes/User.js
+++ front/src/routes/User.js
@@ -6,3 +6,23 @@ import {useQuery} from '@apollo/react-hooks';
 
 function User() {
     const {loading, error, data} = useQuery(queries.getUsers);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
+
+    const result = data.getUsers.map(user => <li key={user.id}>{user.id}, {user.facebookId}, {user.displayName}</li>);
+
+    return (
+        <>
+            <h1>User</h1>
+            <Link to="/">Home</Link>
+            <Link to="/user/1">User 1</Link>
+            <ul>
+                {result}
+            </ul>
+
+        </>
+    );
+}
+
+export default User;
