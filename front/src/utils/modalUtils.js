fc999301d19427e5f66b90f2f1b0c3c8a3d39fc0
fl-air <91276549+fl-air@users.noreply.github.com>
2022-12-04 20:58:46 +0000

    fixing more tests

--- front/src/utils/modalUtils.js
+++ front/src/utils/modalUtils.js
@@ -1,2 +1,5 @@
 function getModalTitle(filterType, filterState) {
-    return Object.entries(filterState).reduce((acc, val) 
+    return Object.entries(filterState).reduce((acc, val) => acc.concat(`${val[0]}:${val[1]} `), '');
+}
+
+export 
